b815d76966b45bb1477c5672e35b19b48f28faed
Manoj Kumar <manoj@aresourcepool.com>
2025-01-04 13:00:16 +0530

    make category

--- database/migrations/2025_01_04_072043_create_categories_table.php
+++ database/migrations/2025_01_04_072043_create_categories_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('brands', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('logo')->nullable();
+            $table->string('image');
             $table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('brands');
+        Schema::dropIfExists('categories');
     }
 };
